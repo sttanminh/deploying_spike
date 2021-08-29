23908ebacaefd450e70554b0b9d0cb8f89bcd5d0
Tan Minh Nguyen <st.tanminh@gmail.com>
2021-08-29 13:48:49 +1000

    daata

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  fetch('https://covid.ourworldindata.org/data/owid-covid-data.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+
+
+//   useEffect(()=>{
+//     d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTwXSqlP56q78lZKxc092o6UuIyi7VqOIQj6RM4QmlVPgtJZfbgzv0a3X7wQQkhNu8MFolhVwMy4VnF/pub?gid=0&single=true&output=csv', function(data){
+//       postcode.push(data.postcode)
+//       population.push(data.population)
+//       active.push(data.active)
+//       cases.push(data.cases) 
+//   })
+
+
+
+//   useEffect(()=>{
+//     console.log(postcode)
+    
+//   },[])
+// },[])
   return (
     <div className="App">
       Hello world,
